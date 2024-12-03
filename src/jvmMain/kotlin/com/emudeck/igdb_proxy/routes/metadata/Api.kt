86138773e274427a4839209bd26e3eb0ef266c2f
KernelPanic <aaronkoonce25@gmail.com>
2024-12-02 20:48:38 -0600

    update

--- src/jvmMain/kotlin/com/emudeck/igdb_proxy/routes/metadata/Api.kt
+++ src/jvmMain/kotlin/com/emudeck/igdb_proxy/routes/metadata/Api.kt
@@ -21,47 +21,48 @@ data class SearchBody(val title: String)
 data class GetBody(val id: Int)
 
 fun Route.api() {
-	val searchCache = Cache.Builder<String, List<MetadataData>>()
-		.expireAfterWrite(1.hours)
+	val searchCache = Cache.Builder<String, List<GameResponse>>()
+		.expireAfterWrite(8.hours)
 		.build()
-	val getCache = Cache.Builder<Int, MetadataData>()
-		.expireAfterWrite(1.hours)
+	val getCache = Cache.Builder<Int, GameResponse>()
+		.expireAfterWrite(24.hours)
 		.build()
 	post<SearchBody>("/search") {
 		val (title) = it
-		if (searchCache.get(title) == null) {
+		val cached = searchCache.get(title)
+		if (cached != null)
+		{
+			call.respond(cached)
+		} else
+		{
 			val response = client.post {
 				url(API_URL)
 				header("Client-ID", auth.clientId)
 				setBody("search \"$title\"; fields *, involved_companies.*, involved_companies.company.*, game_modes.*, multiplayer_modes.*, platforms.*; limit 20;")
 			}.body<List<GameResponse>>()
-
-			val metadata = response.map { element -> element.toMetaDeck() }
+			val metadata = response.sortedBy { x -> x.id }
 			searchCache.put(title, metadata)
+			getCache.put(metadata.first().id, metadata.first())
 			call.respond(metadata)
-		}
-		else
-		{
-			call.respond(searchCache.get(title)!!)
 		}
 	}
 	post<GetBody>("/get") {
 		val (id) = it
-		if (getCache.get(id) == null)
+		val cached = getCache.get(id)
+		if (cached != null)
+		{
+			call.respond(cached)
+		} else
 		{
 			val response = client.post {
 				url(API_URL)
 				header("Client-ID", auth.clientId)
 				setBody("where id = $id; fields *, involved_companies.*, involved_companies.company.*, game_modes.*, multiplayer_modes.*, platforms.*; limit 20;")
 			}.body<List<GameResponse>>()
-			val metadata = response.first().toMetaDeck()
+			val metadata = response.first()
 			println(metadata)
 			getCache.put(id, metadata)
 			call.respond(metadata)
-		}
-		else
-		{
-			call.respond(getCache.get(id)!!)
 		}
 	}
 }
